e0aaf323f4e3aa0ebfeb4c3cefdf0dc0f1bb09ce
Marat Gabitov <marat.kungur@gmail.com>
2021-01-26 19:45:14 +0500

    - Глава 2.2.6

--- webpack/webpack.chapter-02-02.js
+++ webpack/webpack.chapter-02-02.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './sources/chapter-02/index.js',
+    entry: './sources/chapter-02-02/index.js',
     module: {
         rules: [
             { test: /\.css$/, use: ['style-loader', 'css-loader'] }
@@ -17,7 +17,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             hash: true,
             filename: './index.html',
-            template: path.resolve(__dirname, '../sources/chapter-02/index.html')
+            template: path.resolve(__dirname, '../sources/chapter-02-02/index.html')
         })
     ],
 };
