d9b2ecfa6b83944c30d4fe9ac83779348b2baeb2
Marat Gabitov <marat.kungur@gmail.com>
2021-01-31 22:24:05 +0500

    - Глава 3.3

--- react-in-action-2/src/index.js
+++ react-in-action-2/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Greeting from "./chapter-03-01/Greeting";
 // Глава 2. Обзор React
 // import App from './chapter-02-04/App';
 // Листинг 3.2. Использование setState для изменения состояния компонента
@@ -11,6 +10,9 @@ import Greeting from "./chapter-03-01/Greeting";
 // Листинг 3.4. Неизменяемые свойства компонентов React
 // import Counter from "./chapter-03-01/Counter";
 // Листинг 3.5. Функциональные компоненты без состояния
+// import Greeting from "./chapter-03-01/Greeting";
+// Листинг 3.6. Передача свойств от родителя к потоку
+import UserCard from "./chapter-03-01/UserCard";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,7 +20,8 @@ ReactDOM.render(
     {/*<Secret />*/}
     {/*<ShallowMerge />*/}
     {/*<Counter incrementBy={1} />*/}
-    <Greeting for="Mark" />
+    {/*<Greeting for="Mark" />*/}
+    <UserCard username="erondu" />
   </React.StrictMode>,
   document.getElementById('root')
 );
