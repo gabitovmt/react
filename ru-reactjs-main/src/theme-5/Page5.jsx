d6113531de3e462aacbb5405bfcd6b3e2f2ec667
Gabitov <marat.kungur@gmail.com>
2021-02-05 01:14:09 +0500

    - ru.reactjs.org Глава 5. (продолжение)

--- ru-reactjs-main/src/theme-5/Page5.jsx
+++ ru-reactjs-main/src/theme-5/Page5.jsx
@@ -16,20 +16,20 @@ export default class Page5 extends React.Component {
         <FuncClock date={new Date()} />,
         document.getElementById('clock-1')
       )
-    });
+    }, 50);
 
     this.setState({timer});
   }
 
   componentWillUnmount() {
-    console.log('component will unmount');
     clearTimeout(this.state.timer);
     ReactDOM.unmountComponentAtNode(document.getElementById('clock-1'));
   }
 
   render() {
     return <section>
-      <div id="clock-1"/>
+      <div id="clock-1" />
+      <ComponentClock />
     </section>
   }
 }
@@ -39,4 +39,37 @@ function FuncClock(props) {
     <h1>Привет, мир!</h1>
     <h2>Сейчас {props.date.toLocaleTimeString()}</h2>
   </div>
+}
+
+class ComponentClock extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      date: new Date()
+    }
+
+    this.timerId = null;
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(() => this.tick(), 50);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return <div>
+      <h1>Привет, мир!</h1>
+      <h2>Сейчас {this.state.date.toLocaleTimeString()}</h2>
+    </div>
+  }
 }
