30a15a380cad2526693f48005a3a803e14acead2
Gabitov <marat.kungur@gmail.com>
2021-05-21 03:25:44 +0500

    - Jest. Использование сопоставлений

--- refactoring/src/jest-example/02-using-matchers/a.test.js
+++ refactoring/src/jest-example/02-using-matchers/a.test.js
@@ -29,3 +29,56 @@ test('null', () => {
   expect(n).not.toBeTruthy(); // true, если использовать в if
   expect(n).toBeFalsy(); // false, если использовать в if
 });
+
+// Числа
+test('два плюс два', () => {
+  const value = 2 + 2;
+  expect(value).toBeGreaterThan(3);
+  expect(value).toBeGreaterThanOrEqual(3.5);
+  expect(value).toBeLessThan(5);
+  expect(value).toBeLessThanOrEqual(4.5);
+
+  // toBe и toEqual эквивалентны по отношению к числам
+  expect(value).toBe(4);
+  expect(value).toEqual(4);
+});
+
+test('сложение чисел с плавающей запятой', () => {
+  const value = 0.1 + 0.2;
+  // expect(value).toBe(0.3); Это не будет работать из-за ошибки округления
+  expect(value).toBeCloseTo(0.3);
+});
+
+// Строки
+test('в команде нет места Я', () => {
+  expect('команда').not.toMatch(/Я/);
+});
+
+test('но есть "ася" в Васе', () => {
+  expect('Вася').toMatch(/ася/);
+});
+
+// Массивы и перебираемые объекты
+const shoppingList = [
+  'diapers',
+  'kleenex',
+  'trash bags',
+  'paper towels',
+  'milk'
+];
+test('the shopping list has milk on it', () => {
+  expect(shoppingList).toContain('milk');
+  expect(new Set(shoppingList)).toContain('milk');
+});
+
+function compileAndroidCode() {
+  throw new Error('you are using the wrong JDK');
+}
+test('compiling android goes as expected', () => {
+  expect(() => compileAndroidCode()).toThrow();
+  expect(() => compileAndroidCode()).toThrow(Error);
+
+  // You can also use the exact error message or a regexp
+  expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK');
+  expect(() => compileAndroidCode()).toThrow(/JDK/);
+});
