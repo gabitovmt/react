6885c5f9543c7fb3c2e3c62dba474dd3fa768520
Marat Gabitov <gabitovmt@yandex.ru>
2024-05-11 15:35:40 +0500

    Неизменяемые свойства компонентов React

--- react-in-action/ch3/src/App.js
+++ react-in-action/ch3/src/App.js
@@ -1,54 +1,72 @@
+import './App.css';
 import React, {Component} from 'react';
-import Secret from "./Secret/Secret";
-import ShallowMerge from "./ShallowMerge/ShallowMerge";
-import Parent from "./Parent/Parent";
+import Secret from './Secret/Secret';
+import ShallowMerge from './ShallowMerge/ShallowMerge';
+import Parent from './Parent/Parent';
+import Counter from './Counter/Counter';
 
-const PAGE_NONE = 'NONE';
-const PAGE_L3_2 = 'PAGE-LISTING-3.2';
-const PAGE_L3_3 = 'PAGE-LISTING-3.3';
-const PAGE_EX3_2 = 'PAGE-EX-3.2'
+const pages = [{
+  id: 'l-3.2',
+  label: 'Листинг 3.2. setState()',
+  component: <Secret/>
+}, {
+  id: 'l-3.3',
+  label: 'Листинг 3.3. setState()',
+  component: <ShallowMerge/>
+}, {
+  id: 'ex-3.2',
+  label: 'Упражнение 3.2. setState()',
+  component: <Parent/>
+}, {
+  id: 'l-3.4',
+  label: 'Листинг 3.4. Неизменяемые свойства',
+  component: <Counter onIncrement={console.log}/>
+}]
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pageId: PAGE_NONE
+      page: null
     };
     this.onClick = this.onClick.bind(this);
+    this.onBackClick = this.onBackClick.bind(this);
   }
 
   onClick(event) {
     this.setState(() => ({
-      pageId: event.target.value
+      page: pages.find(page => page.id === event.target.value)
+    }));
+  }
+
+  onBackClick() {
+    this.setState(() => ({
+      page: null
     }));
   }
 
   buttons() {
-    if (this.state.pageId === PAGE_NONE) {
-      return (
-        <>
-          <button onClick={this.onClick} value={PAGE_L3_2}>Листинг 3.2. setState()</button>
-          <button onClick={this.onClick} value={PAGE_L3_3}>Листинг 3.3. setState()</button>
-          <button onClick={this.onClick} value={PAGE_EX3_2}>Упражнение 3.2. setState()</button>
-        </>
-      );
+    if (this.state.page != null) {
+      return <button onClick={this.onBackClick}>Назад</button>;
     }
 
-    return <button onClick={this.onClick} value={PAGE_NONE}>Назад</button>;
+    return (
+      <>
+        {
+          pages.map((page) =>
+            <button key={page.id} onClick={this.onClick} value={page.id}>{page.label}</button>
+          )
+        }
+      </>
+    );
   }
 
   page() {
-    switch (this.state.pageId) {
-      case PAGE_L3_2:
-        return <Secret/>;
-      case PAGE_L3_3:
-        return <ShallowMerge/>;
-      case PAGE_EX3_2:
-        return <Parent/>;
-      case PAGE_NONE:
-      default:
-        return null;
+    if (this.state.page == null) {
+      return null;
     }
+
+    return this.state.page.component;
   }
 
   render() {
---
+++ react-in-action/ch3/src/Counter/Counter.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+    this.onButtonClick = this.onButtonClick.bind(this);
+  }
+
+  onButtonClick() {
+    this.setState(function(prevState, props) {
+      return { count: prevState.count + props.incrementBy };
+    });
+  }
+
+  componentDidUpdate() {
+    this.props.onIncrement(this.state.count);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.onButtonClick}>++</button>
+      </div>
+    );
+  }
+}
+
+Counter.propTypes = {
+  incrementBy: PropTypes.number,
+  onIncrement: PropTypes.func.isRequired
+};
+
+Counter.defaultProps = {
+  incrementBy: 1
+};
+
+export default Counter;
